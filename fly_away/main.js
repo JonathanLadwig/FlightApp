0bb3221d326b634f8d7cc10766642bddd75f7d8c
JonathanLadwig <jonladwig98@gmail.com>
2023-02-15 15:46:09 +0200

    style: :art: Sidebar and Polling
    Added a disappearing sidebar (actualy content to be added later) and added polling function.
    
    Polling does NOT delete the last marker.

--- fly_away/main.js
+++ fly_away/main.js
@@ -4,7 +4,16 @@ import axios from "axios";
 var latitude = 80;
 var longitude = 1;
 const apiurl = 'https://api.wheretheiss.at/v1/satellites/25544';
+var map = L.map('map', {
+    maxZoom: 10,
+    minZoom: 2,
+    zoomControl: false
+});
+getPos();
+drawMap(latitude,longitude);
 
+//API
+function getPos(){
 axios
 .get(apiurl)
 // .then((responseJSON) => console.log(responseJSON))
@@ -14,14 +23,16 @@ axios
         longitude = responseJSON.data.longitude;
         console.log(latitude);
         console.log(longitude);
-        drawMap(latitude, longitude)
+        // drawMap(latitude, longitude)
     // }
     // document.getElementById('app').appendChild(flights)
 })
+}
 // .catch((error) => console.error(error))
 
+//drawing the map
 function drawMap(latitude, longitude){
-    var map = L.map('map').setView([latitude, longitude], 5);
+    map.setView([latitude, longitude], 5);
 
     var marker = L.marker([latitude, longitude]).addTo(map);
 
@@ -30,14 +41,19 @@ function drawMap(latitude, longitude){
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
 
+        L.control.zoom({
+            position: 'bottomright'
+        }).addTo(map);
+
     marker.bindPopup("<b>ISS Location:</b>").openPopup();
 }
 
-// var map = L.map('map').setView([latitude, longitude], 13);
+function redrawMap(latitude, longitude){
+    map.setView([latitude, longitude], 5);
+    marker = L.marker([latitude, longitude]).addTo(map);
+}
 
-// var marker = L.marker([latitude, longitude]).addTo(map);
-
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
+var intervalId = window.setInterval(function(){
+    getPos();
+    redrawMap(latitude, longitude);
+  }, 10000);
