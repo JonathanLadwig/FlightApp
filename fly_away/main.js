3dd4bce938625db63251dc5c6342893085c1d9c2
JonathanLadwig <jonladwig98@gmail.com>
2023-02-16 14:20:41 +0200

    feat: :sparkles: Added Markers for Each Flight & Added Each FLight to Sidebar
    Zooms automatically to the ISS and first marker is slow to show.

--- fly_away/main.js
+++ fly_away/main.js
@@ -1,82 +1,92 @@
-import './styles.scss'
 import axios from "axios";
-import { latLng } from 'leaflet';
+import "./styles.scss";
 
 var latitude = 0;
 var longitude = 0;
 const flightData = [];
-const apiurl = 'https://api.wheretheiss.at/v1/satellites/25544';
-const openskyURL = 'https://opensky-network.org/api/states/all';
+const apiurl = "https://api.wheretheiss.at/v1/satellites/25544";
+const openskyURL = "https://opensky-network.org/api/states/all";
 
-//setPos();
+// drawMap(latitude, longitude);
+setPos();
 setISSPos();
 
-var map = L.map('map', {
-    maxZoom: 10,
-    minZoom: 2,
-    zoomControl: false
+var map = L.map("map", {
+  maxZoom: 10,
+  minZoom: 2,
+  zoomControl: false,
 });
-drawMap(latitude,longitude);
-var markerISS = L.marker([latitude, longitude]).addTo(map).on('mouseover', onClick);
+drawMap(latitude, longitude);
+
+var markerISS = L.marker([latitude, longitude])
+  .addTo(map)
+  .on("mouseover", onClick);
 markerISS.bindPopup("<b>ISS Location:</b>").openPopup();
 
 // OpenSkyAPI
-function setPos(){
-axios
-.get(openskyURL)
-.then((responseJSON) => {
-    // for (const flight of responseJSON.data.states){
-    //     console.log(responseJSON.data.states[flight]);
-    //     flightData[flight] = responseJSON.data.states[flight];
-    // }
+function setPos() {
+  axios.get(openskyURL).then((responseJSON) => {
     for (var i = 0; i < 20; i++) {
-        console.log(responseJSON.data.states[i]);
-        flightData[i] = responseJSON.data.states[i];
-        marker = new L.marker([flightData[i].latitude, flightData[i].longitude])
-        .bindPopup(locations[i][0])
-        .addTo(map);
+      if (
+        responseJSON.data.states[i][6] !== null &&
+        responseJSON.data.states[i][5] !== null
+      ) {
+        //Add it as a marker
+        L.marker([
+          responseJSON.data.states[i][6],
+          responseJSON.data.states[i][5],
+        ])
+          .addTo(map)
+          .on("mouseover", onClick);
+
+        //Add it as a button to the sidebar
+        const flightbutt = document.createElement("button");
+        flightbutt.innerHTML = `<button>${responseJSON.data.states[i][1]}</button>`;
+        document.getElementById("mySidebar").appendChild(flightbutt);
+      }
     }
-});
+  });
 }
 
 //ISSAPI
-function setISSPos(){
-axios
-.get(apiurl)
-.then((responseJSON) => {
-        latitude = responseJSON.data.latitude;
-        longitude = responseJSON.data.longitude;
-});
+function setISSPos() {
+  axios.get(apiurl).then((responseJSON) => {
+    latitude = responseJSON.data.latitude;
+    longitude = responseJSON.data.longitude;
+  });
 }
 
 //drawing the map
-function drawMap(latitude, longitude){
-    map.setView([latitude, longitude], 2);
-    //markerISS = L.marker([latitude, longitude]).addTo(map);
+function drawMap(latitude, longitude) {
+  map.setView([latitude, longitude], 2);
+  //markerISS = L.marker([latitude, longitude]).addTo(map);
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
 
-        L.control.zoom({
-            position: 'bottomright'
-        }).addTo(map);
+  L.control
+    .zoom({
+      position: "bottomright",
+    })
+    .addTo(map);
 }
 
 //redrawing the ISS
-function redrawMap(latitude, longitude){
-    //map.setView([latitude, longitude], 5);
-    var latLng = new L.LatLng(latitude, longitude);
-    markerISS.setLatLng(latLng)
+function redrawMap(latitude, longitude) {
+  //map.setView([latitude, longitude], 5);
+  var latLng = new L.LatLng(latitude, longitude);
+  markerISS.setLatLng(latLng);
 }
 
 //CLicking a marker
-function onClick(){
-    map.setView([latitude, longitude], 5); 
+function onClick() {
+  map.setView([latitude, longitude], 2);
 }
 
-var intervalId = window.setInterval(function(){
-    setISSPos();
-    redrawMap(latitude, longitude);
-  }, 1000);
+var intervalId = window.setInterval(function () {
+  setISSPos();
+  redrawMap(latitude, longitude);
+}, 1000);
