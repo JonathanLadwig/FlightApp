4473f3c7a8ee66946bfdcc170940f6a292754e0d
JonathanLadwig <jonladwig98@gmail.com>
2023-02-23 08:05:19 +0200

    build: :heavy_plus_sign: Added TailwindCSS

---
+++ fly_away/navigationBar.js
@@ -0,0 +1,13 @@
+export { openNav, closeNav };
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "30vw";
+  document.getElementById("main").style.marginLeft = "30vw";
+  document.getElementById("sidebutt").style.color = "rgba(0, 0, 0, 0)";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("sidebutt").style.color = "rgba(0, 0, 0, 1)";
+}
