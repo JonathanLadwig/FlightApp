1cca5cd5c3f955dc518b3917fdc1a3f67a5c7d47
JonathanLadwig <jonladwig98@gmail.com>
2023-03-03 16:09:56 +0200

    refactor: :fire: Removed Unused Functions

--- fly_away/mapModule.ts
+++ fly_away/mapModule.ts
@@ -5,7 +5,6 @@ import { IFlight, IFlightMarker } from "./src/models/IFlight";
 
 const buttList = document.getElementById("buttList");
 let flightMarkers: IFlightMarker[];
-let selectedFlight: IFlightMarker;
 
 //Initialising the leaflet map
 export const map = L.map("map", {
@@ -41,7 +40,7 @@ function setFlightMarkers(flight: IFlight, i: number) {
     .bindPopup(`Callsign: ${flight.callsign} <br/> Origin: ${flight.origin} <br/> Baro-Altitude: ${Math.round(flight.baro_altitude / 10) / 100}km`)
     .on("mouseover", () => flightMarkers[i].planeMarker.openPopup())
     .on("mouseout", () => flightMarkers[i].planeMarker.closePopup())
-    .on("click", () => flyToOnClick(flight.latitude, flight.longitude))
+    .on("click", () => flyTo(flightMarkers[i]))
   flightMarkers.push(flightMarker);
 }
 
@@ -54,12 +53,6 @@ function createNewFlightButt(flight: IFlight) {
     getPosFromCallsign(flightButt.innerText)
   );
   if (buttList) buttList.appendChild(flightButt);
-}
-
-function removeLowerCase(str: string): string {
-  let pattern = new RegExp("[a-z]", 'g');
-  str = str.replace(pattern, "");
-  return str.replace(" ", "");
 }
 
 //drawing the map
@@ -83,94 +76,15 @@ function drawMap(map: L.Map) {
 function getPosFromCallsign(callsign: string) {
   for (let flight of flightMarkers) {
     if (flight && flight.plane.callsign.includes(callsign)) {
-      flyToOnClick(flight.plane.latitude, flight.plane.longitude);
+      flyTo(flight);
     }
   }
 }
-
-//CLicking a marker or button will zoom in on it
-function flyToOnClick(lat: number, long: number) {
-  map.flyTo([lat, long], 8);
+//CLicking a marker or button will zoom in on it and open the popup
+function flyTo(flight: IFlightMarker) {
+  map.flyTo([flight.plane.latitude, flight.plane.longitude], 8);
+  flight.planeMarker.openPopup();
 }
 
-function getAircraftType(typeNum: number): L.DivIcon {
-  const aircraftIcon = new L.DivIcon({
-    iconUrl: "./images/planeicon.png",
-    iconRetinaUrl: "./images/planeicon.png",
-    iconSize: [38, 95],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-    shadowUrl: './images/planeicon.png',
-    shadowSize: [68, 95],
-    shadowAnchor: [22, 94]
-  });
-  //Map it to an enum instead maybe?
-  switch (typeNum) {
-    case 1:
-      console.log("No ADS-B Emitter Category Information.");
-      break;
-    case 2:
-      console.log("Light.");
-      break;
-    case 3:
-      console.log("Small.");
-      break;
-    case 4:
-      console.log("Large");
-      break;
-    case 5:
-      console.log("High Vortex Large.");
-      break;
-    case 6:
-      console.log("Heavy.");
-      break;
-    case 7:
-      console.log("High Performance.");
-      break;
-    case 8:
-      console.log("Rotorcraft.");
-      break;
-    case 9:
-      console.log("Glider.");
-      break;
-    case 10:
-      console.log("Blimp.");
-      break;
-    case 11:
-      console.log("Parachutist.");
-      break;
-    case 12:
-      console.log("Ultralight.");
-      break;
-    case 13:
-      console.log("Reserved.");
-      break;
-    case 14:
-      console.log("UAV.");
-      break;
-    case 15:
-      console.log("Spacecraft.");
-      break;
-    case 16:
-      console.log("Emergency Vehicle.");
-      break;
-    case 17:
-      console.log("Service Vehicle.");
-      break;
-    case 18:
-      console.log("Point Obstacle Vehicle.");
-      break;
-    case 19:
-      console.log("Cluster Obstacle.");
-      break;
-    case 20:
-      console.log("Line Obstacle.");
-      break;
-    default:
-      console.log("No info");
-      break;
-  }
-  return aircraftIcon;
-}
+export { drawMap, getPosFromCallsign };
 
-export { drawMap, getPosFromCallsign, flyToOnClick };
